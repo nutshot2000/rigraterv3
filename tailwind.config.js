4d893db6bda60d5da96fbd4050e84055e6f55361
Jimmy <jbattisson@gmail.com>
2025-10-26 23:27:08 +0000

    style(ui): set product modal to intermediate width

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './App.tsx',
-    './components/**/*.{ts,tsx}',
-    './pages/**/*.{ts,tsx}',
-    './context/**/*.{ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
-  plugins: [],
-};
+  theme: {
+    extend: {
+      maxWidth: {
+        'intermediate': '960px',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
 
 
