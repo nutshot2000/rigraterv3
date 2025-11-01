7c5b29ca7c0a2e5d86a85a8f92b4aeeabab19d7c
Jimmy <jbattisson@gmail.com>
2025-11-01 15:39:34 +0000

    ux: scroll to top on route change

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Suspense, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AppProvider, useApp } from './context/AppContext';
 import Header from './components/public/Header';
 import Footer from './components/public/Footer';
@@ -25,12 +25,21 @@ const AdminGate: React.FC = () => {
     );
 };
 
+const ScrollToTop: React.FC = () => {
+    const { pathname, search } = useLocation();
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    }, [pathname, search]);
+    return null;
+};
+
 const App: React.FC = () => {
     return (
         <HelmetProvider>
             <GoogleAnalytics />
             <AppProvider>
                 <BrowserRouter>
+                    <ScrollToTop />
                     <div className="flex flex-col min-h-screen text-gray-100 theme-blueprint bg-grid bg-noise bg-crt-dark">
                         <Header />
                         <main className="flex-grow container mx-auto px-4 py-8">
